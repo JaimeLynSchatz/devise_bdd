915ca0e085c847aaea93f2fa5f821fca6723c48c
Philip Crawford <philipharry@gmail.com>
2014-02-04 15:18:20 -0600

    Add test for ability to sign up

---
+++ spec/features/anonymous_can_sign_up_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'Anonymous users' do
+  background do
+    @user = build :user
+  end
+
+  scenario 'can sign up' do
+    visit '/'
+
+    click_link_or_button 'Sign up'
+
+    fill_in 'Email', :with => @user.email
+    fill_in 'user_password', :with => @user.password
+    fill_in 'user_password_confirmation', :with => @user.password
+
+    click_link_or_button 'Sign up'
+
+    expect(page).to have_content 'Welcome!'
+  end
+end
